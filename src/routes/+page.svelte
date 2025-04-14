d2d9b122a1c4390c112bda0b5cf52fd602d67362
Klee Thomas <klee.ut@gmail.com>
2025-04-15 07:54:06 +1000

    :tada: super basic website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<h1>Oh No!</h1>
+<h2>Something is wrong with our website</h2>
+<h2>SlashNEW.tech will be back soon</h2>
+<p>SlashNEW is still running on May 28 and 29 our website is just down.</p>
+<div class="p1">
+	<p>
+		<a class="button" href="https://crowdcatcher.co/e/..new-conference-2025-tickets-iSg83WjEwR">
+			Buy Tickets
+		</a>
+	</p>
+</div>
+
+<p>
+	If you need to contact the organisers please email us at <a
+		href="mailto:conference@slashnew.tech"
+	>
+		conference@slashnew.tech
+	</a>
+</p>
+
+<style>
+	:global(body) {
+		color: #fff;
+		font-family: 'Courier New', Courier, monospace;
+		line-height: 1.5rem;
+		text-align: center;
+		padding: 0;
+	}
+	:global(html) {
+		background: #000;
+		background: linear-gradient(
+			90deg,
+			rgba(0, 0, 0, 1) 0%,
+			rgba(0, 31, 29, 1) 50%,
+			rgba(0, 0, 0, 1) 100%
+		);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		height: 100vh;
+		padding: 0;
+	}
+	a.button {
+		border: 0;
+		background: magenta;
+		color: black;
+		padding: 1rem;
+		margin: 1rem;
+		text-decoration: none;
+		font-size: 1.5rem;
+	}
+	a {
+		color: magenta;
+		font-weight: bold;
+	}
+	div.p1 {
+		padding: 1rem;
+	}
+</style>
