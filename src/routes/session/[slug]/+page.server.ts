c01790b22856407c587764228555d3f7f32faa7b
Klee Thomas <klee.ut@gmail.com>
2025-04-19 18:03:08 +1000

    :construction: speaker page WIP

--- src/routes/session/[slug]/+page.server.ts
+++ src/routes/session/[slug]/+page.server.ts
@@ -1,8 +1,19 @@
+import * as sessions from '$lib/data/sessions';
 import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params }) => {
-	const { slug } = params;
+	const session = Object.values(sessions).find((session) => {
+		if (session.slug === params.slug) {
+			return true;
+		}
+	});
+	if (!session) {
+		console.error(`Session with slug ${params.slug} not found`);
+		redirect(302, '/');
+	}
+	console.log('session', session);
 	return {
-		slug
+		session
 	};
 };
---
+++ src/routes/session/[slug]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { PlannedSession } from '$lib/data/sessions/session';
+
+	const { data } = $props();
+	const { session } = data;
+</script>
+
+<a href="/" data-sveltekit-reload>Agenda</a>
+<h1>{session.title}</h1>
+<p>{session.abstract}</p>
+<div>
+	<h2>Speakers</h2>
+	<ul>
+		{#each session.speakers as speaker}
+			<div>
+				<h3>{speaker.name}</h3>
+				<p>{speaker.bio}</p>
+			</div>
+		{/each}
+	</ul>
+</div>
