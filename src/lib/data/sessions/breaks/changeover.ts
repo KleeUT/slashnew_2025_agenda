c29e02b4b92200840d61fdbb3e39cc98cb8b340e
Klee Thomas <klee.ut@gmail.com>
2025-04-21 21:38:39 +1000

    :speech_balloon: updating break copy

--- src/lib/data/sessions/breaks/changeover.ts
+++ src/lib/data/sessions/breaks/changeover.ts
@@ -3,6 +3,7 @@ import type { Session } from '../session';
 export const changeover: Session = {
 	slug: 'changeover',
 	title: '5 Minute Session Change Over',
-	abstract: 'Quick break so you can move between sessions',
+	abstract:
+		'Quick break so you can move between sessions. No coffee or food will be served during this time.',
 	speakers: []
 };
--- src/lib/data/sessions/breaks/lunch.ts
+++ src/lib/data/sessions/breaks/lunch.ts
@@ -3,6 +3,7 @@ import type { Session } from '../session';
 export const lunch: Session = {
 	slug: 'lunch',
 	title: 'Lunch',
-	abstract: "Time for a longer break we'll have lunch served in the sponsor hall",
+	abstract:
+		'Time for a longer break. Lunch will be an hour long to give you time to grab food from the sponsor hall and find some new friends to chat with while you eat. Pirate Coffee will be serving up coffee during this time.',
 	speakers: []
 };
