341a7c189ddf5211b73401cd9ccad3cdb1afb599
Klee Thomas <klee.ut@gmail.com>
2025-04-21 21:36:50 +1000

    :speech_balloon: add conference feud outline

--- src/lib/data/sessions/speakers/day1/conference-feud.ts
+++ src/lib/data/sessions/speakers/day1/conference-feud.ts
@@ -4,7 +4,9 @@ export const conferenceFeud: Session = {
 	slug: 'conference-feud',
 	title: 'Conference Feud',
 	abstract: `
-  # Place holder
+Come along for some mid conference fun. We'll be playing a game of Family Feud with the speakers vs Newcastle locals to see who knows the crowd better. 
+There will be QR Codes around the venue for you to fill out the survey during the day. 
+This session will not be recorded, so the speakers can get up and speak their mind without fear of it being shared online.
   `,
 	speakers: []
 };
