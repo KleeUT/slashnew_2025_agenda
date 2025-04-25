d6d2fa9fdee9bf2cbeb40d1ddb06f77a27cb9b05
Klee Thomas <klee.ut@gmail.com>
2025-04-25 12:38:56 +1000

    :sparkle: loading the agenda on day 2 will show day 2

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import type { PageServerLoad } from './$types';
 import { agenda } from '$lib/data/agenda';
 export const load: PageServerLoad = async () => {
-	const day = agenda.day1;
+	const today = new Date();
+	const day = today.getTime() < agenda.day2.date.getTime() ? agenda.day1 : agenda.day2;
 	return {
 		day
 	};
