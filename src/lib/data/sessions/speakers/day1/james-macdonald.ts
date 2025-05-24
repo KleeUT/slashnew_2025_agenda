b5e277badefdf3def8cb110c8f01a49aba0bb997
Klee Thomas <klee.ut@gmail.com>
2025-05-24 14:28:29 +1000

    :wrench: add Nish Chandran as part of the panel

--- src/lib/data/sessions/speakers/day1/james-macdonald.ts
+++ src/lib/data/sessions/speakers/day1/james-macdonald.ts
@@ -16,9 +16,13 @@ export const jamesMacDonald: Session = {
 			jobTitle: 'Managing Director at NTP'
 		},
 		{
-			name: 'Andrew Cresp',
+			name: 'Nish Chandran',
 			bio: `
+Nish Chandran is a senior technology and digital executive with 28 years’ experience across Financial Services, Banking, Insurance, Payments, Consulting and Software Engineering.
 
+Over the last 10 years has held various CIO and Senior leadership roles in at ANZ, Suncorp Bank, MLC Life Insurance, FirstData (Fiserv), RBA and CBA. Nish is passionate about delivering business outcomes and is known for his customer obsession, commitment to quality, innovation and nurturing talent. Nish is currently the Chief Technology Officer (CTO) at NGM Group since Dec 2024.
+
+Having successfully executed multiple business transformations within culturally diverse organisations and regions, Nish is always looking to leverage his experiences to help organisations drive further growth and is committed to lifelong learning.
       `,
 			image: 'andrew-cresp.jpg',
 			jobTitle: 'CIO at NGM Group'
@@ -28,11 +32,7 @@ export const jamesMacDonald: Session = {
 			bio: `
 Samantha Davies is the Cloud and Infrastructure Centre of Expertise (CCOE) Lead at Telstra.  Sam leads multiple teams across Cloud Security, Service Management, Automation and Cloud Economics and is a key strategy lead for the cloud and infrastructure capability in Telstra. 
 
-
-
 Sam joined Telstra almost 3 years ago and prior to that she was with ANZ Bank for 11 years where she was the key lead in their Cloud Simplification and Transformation program.  Prior to that she was the ANZ Cyber Defence Lead where she managed teams across multiple regions protecting ANZ bank on a day to day basis.  
-
- 
 
 Samantha has lived and worked in seven (7) countries during her career in technology and started out as a software developer, database administrator and development lead.  Sam is known for her passion and positivity and is often referred to as the “glue” who brings teams together.  Her passion for diversity in technology has led to her starting a not-for-profit called Tech Exec Women (TEW) with her co-director Leigh Gibson which supports, connects and empowers women leaders in technology.
       `,
